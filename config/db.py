1cb6b2dd2b282124ca49bdb454f38d6ca593fae1
josarta <josarta@misena.edu.co>
2024-07-06 09:50:29 -0500

    feat: add config Connections and Transactions

--- config/db.py
+++ config/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
 
-if settings.MODE == 'DEV':
+if settings.MODE == 'PRO':
   DATABASE_HOST = "bt2gtprzkoxalty9qk10-mysql.services.clever-cloud.com"
   DATABASE_NAME = "bt2gtprzkoxalty9qk10"
   DATABASE_PORT = "3306"
