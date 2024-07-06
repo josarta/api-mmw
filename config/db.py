bfb3bcb9bfaee416df0328cc1c4e36ccfaa47a6d
josarta <josarta@misena.edu.co>
2024-07-06 09:45:13 -0500

    feat: add congig Connections and Transactions

--- config/db.py
+++ config/db.py
@@ -17,7 +17,7 @@ else:
   DATABASE_USERNAME = "sgroot" 
   DATABASE_PASSWORD = "M7AS14lw5MfxG,YV"
 
-engine = create_engine('mysql+pymysql://'+DATABASE_USERNAME+':'+DATABASE_PASSWORD+'@'+DATABASE_HOST+':'+DATABASE_PORT+'/'+ DATABASE_NAME,echo=False, pool_size=10, max_overflow=20)
+engine = create_engine('mysql+pymysql://'+DATABASE_USERNAME+':'+DATABASE_PASSWORD+'@'+DATABASE_HOST+':'+DATABASE_PORT+'/'+ DATABASE_NAME,echo=False, pool_size=30, max_overflow=-1)
 meta = MetaData()
 meta.reflect(bind=engine)
 
