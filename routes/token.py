ff401b250992ccb7ec35ec125a57a1dce9542377
josarta <josarta@misena.edu.co>
2024-07-08 10:42:26 -0500

    feat: remove if login token before call endpoint

--- routes/token.py
+++ routes/token.py
@@ -97,10 +97,9 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        if settings.MODE == 'DEV':
-         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-         username: str = payload.get("sub")
-         if username is None:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
     except InvalidTokenError:
